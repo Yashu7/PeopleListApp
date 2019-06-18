4b9230770bdac3b1ecb2a83d1a61b734c99d1bb3
JanMalawski <janmalawski2@gmail.com>
2019-06-18 18:15:02 +0200

    Try/Catch for input.

--- PeopleListApp/Options.cs
+++ PeopleListApp/Options.cs
@@ -28,7 +28,16 @@ namespace PeopleListApp
                 Console.WriteLine("1.Create new entry into the database.");
                 Console.WriteLine("2.Delete entry");
                 Console.WriteLine("3.Exit");
-                MenuOption = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter correct option number: ");
+                try
+                {
+                    MenuOption = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Option doesn't exist, please try again. (Options 1-3)");
+                    MenuOption = Convert.ToInt32(Console.ReadLine());
+                }
             } while (MenuOption == 0);
             return (MenuOption);
             
